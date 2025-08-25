fe11f32ed8a98b024cb003aa69bdbe38596bebc2
RamandeepSingh <raman111092@gmail.com>
2025-08-25 22:37:37 +0530

    Created terraform file with multiple blocks

---
+++ terraform-in-hindi-gaurav-sharma/lecture-3/multiple-output-blocks.tf
@@ -0,0 +1,9 @@
+output "first_output_block" {
+  value = "This is the first output block"
+}
+output "second_output_block" {
+  value = "This is the second output block"
+}
+output "third_output_block" {
+  value = "This is the third output block"
+}
