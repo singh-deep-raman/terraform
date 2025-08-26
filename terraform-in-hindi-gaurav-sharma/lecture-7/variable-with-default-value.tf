cb6f3af3b50ac5180ab113a6b0b3f371a2cca2a2
RamandeepSingh <raman111092@gmail.com>
2025-08-26 11:14:59 +0530

    Setting default values to the variables

---
+++ terraform-in-hindi-gaurav-sharma/lecture-7/variable-with-default-value.tf
@@ -0,0 +1,3 @@
+output "print_user_info" {
+  value = "Hello ${var.username}, your email id is ${var.emailId}"
+}
---
+++ terraform-in-hindi-gaurav-sharma/lecture-7/variables.tf
@@ -0,0 +1,6 @@
+variable "username" {
+    default = "Unknown" # If value is passed from command line then that value will be considered instead of default value.
+}
+variable "emailId" {
+    default = "Unknown@gmail.com" # If value is passed from command line then that value will be considered instead of default value.
+}
