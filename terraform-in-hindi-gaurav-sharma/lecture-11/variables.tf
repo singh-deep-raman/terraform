dd6099ef070bad5b0a126a828b3e485163f8c209
RamandeepSingh <raman111092@gmail.com>
2025-08-26 23:25:51 +0530

    adding cmd syntax to input map value

--- terraform-in-hindi-gaurav-sharma/lecture-11/variables.tf
+++ terraform-in-hindi-gaurav-sharma/lecture-11/variables.tf
@@ -7,3 +7,7 @@ variable "user_age_map" {
     rohan  = 28
   }
 }
+# How to pass map values via cmd
+# terraform apply -var 'user_age_map={gaurav=24,ankit=26,rohan=28}'
+# or
+# terraform plan -var 'user_age_map={"abc":10, "bcd":20, "cde":15}'
