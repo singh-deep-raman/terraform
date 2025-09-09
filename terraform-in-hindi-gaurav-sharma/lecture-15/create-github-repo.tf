88d2ccc0e7c4e34c5495bcfd799eae64de71c9bc
RamandeepSingh <raman111092@gmail.com>
2025-09-09 16:45:36 +0530

    Added some more useful terraform commands

--- terraform-in-hindi-gaurav-sharma/lecture-15/create-github-repo.tf
+++ terraform-in-hindi-gaurav-sharma/lecture-15/create-github-repo.tf
@@ -17,6 +17,10 @@ resource "github_repository" "first_repo_in_github_using_terraform" {
     # terraform providers --> to check the providers used in the current configuration
     # file <filename>  --> to check the type of file
     # terraform destroy
+    # terraform destroy --target=github_repository.first_repo_in_github_using_terraform --auto-approve (to delete one specific resource)
     # terraform apply --auto-approve 
+    # terraform validate (to check if the configuration is valid or not)
+    # terraform show (to show the content of the .tfstate file)
+    # terraform refresh (when a resource is changed outside terraform, this command will update the state file, for e.g. you changed the description of the github repository manually and then want that change in .tfstate file)
 
 
