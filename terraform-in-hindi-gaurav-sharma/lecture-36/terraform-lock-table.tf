fb9d53cf99dbb578b76549c14f0365af79ad9813
RamandeepSingh <raman111092@gmail.com>
2025-11-13 21:26:25 +0530

    terraform state file locking using dynamodb

---
+++ terraform-in-hindi-gaurav-sharma/lecture-36/terraform-lock-table.tf
@@ -0,0 +1,9 @@
+terraform {
+#   Create terraform with below backend, run terraform apply and then comment this code and run terraform apply again
+  backend "s3" {
+    bucket = "terraform-state-bucket-raman"
+    key    = "terraform.tfstate" # name of the tfstate file in the bucket
+    region = "ap-south-1"
+    dynamodb_table = "raman-state-lock" # for state locking
+  }
+}
